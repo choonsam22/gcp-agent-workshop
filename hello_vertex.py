bba73467ba048bfa610bd352aa5f6068cb9310e7
choonsam22 <148427160+choonsam22@users.noreply.github.com>
2025-08-22 15:42:50 +0900

    hello_vertex.py

--- hello_vertex.py
+++ hello_vertex.py
@@ -3,10 +3,13 @@ from vertexai.generative_models import GenerativeModel
 import os
 
 project_id = os.environ.get("GOOGLE_CLOUD_PROJECT")
-region = os.environ.get("REGION", "asia-northeast3")
+# 서울 리전 대신 global 지정
+location = os.environ.get("REGION", "asia-northeast3")
+if model_name.startswith("gemini-2.5"):
+    location = "global"
 
-init(project=project_id, location=region)
-model = GenerativeModel("gemini-1.5-flash")
-
+init(project=project_id, location=location)
+# 더 작은 모델로 전환
+model = GenerativeModel("gemini-2.5-flash-lite")
 resp = model.generate_content("안녕! 넌 누구야? 한 줄로 답해줘")
 print(resp.text)
