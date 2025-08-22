5ee7d1a76e1cfe9be33c4507a965d603ed00c744
choonsam22 <148427160+choonsam22@users.noreply.github.com>
2025-08-22 15:54:41 +0900

    Update hello_vertex.py

--- hello_vertex.py
+++ hello_vertex.py
@@ -2,11 +2,9 @@ from vertexai import init
 from vertexai.generative_models import GenerativeModel
 import os
 
-project_id = os.environ.get("GOOGLE_CLOUD_PROJECT")
-
 vertexai.init(project=my-project-40086', location='us-central1')
 
-model = GenerativeModel("gemini-1.5-flash")
+model = GenerativeModel("gemini-1.5-pro")
 
 resp = model.generate_content("안녕! 넌 누구야? 한 줄로 답해줘")
 
